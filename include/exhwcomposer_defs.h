e2fca55ba5c0007b07d787c5b115af354a715ca6
prototype74 <prototype74@outlook.de>
2026-04-24 20:50:06 +0200

    hwcomposer: add missing HWC defines for TARGET_USES_QCOM_BSP (QTI_BSP)
    Add exhwcomposer_defs.h with definitions required by targets using QTI_BSP but
    not present in upstream hwcomposer headers.

---
+++ include/exhwcomposer_defs.h
@@ -0,0 +1,11 @@
+
+#ifndef EX_HWC_DEFS_H
+#define EX_HWC_DEFS_H
+
+#include <hardware/hwcomposer_defs.h>
+
+#define HWC_EVENT_ORIENTATION 1 // HWC_EVENT_VSYNC = 0
+#define HWC_BLIT 6
+#define HWC_SCREENSHOT_ANIMATOR_LAYER 0x00000004
+
+#endif
