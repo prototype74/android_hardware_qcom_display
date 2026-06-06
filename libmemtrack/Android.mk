1f6bf6501a3c2908bafcc7b1dd1af0a5806c3ab4
prototype74 <prototype74@outlook.de>
2026-06-06 11:33:25 +0200

    display: build display HAL modules as vendor modules
    Install display HAL components under /vendor so they link against the vendor
    VNDK and remain isolated from system-side dependencies.

--- libmemtrack/Android.mk
+++ libmemtrack/Android.mk
@@ -24,4 +24,5 @@ LOCAL_CFLAGS := -Wconversion -Wall -Werror -Wno-sign-conversion
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := memtrack_msm.c kgsl.c
 LOCAL_MODULE := memtrack.$(TARGET_BOARD_PLATFORM)
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -3,6 +3,7 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
+LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
@@ -28,6 +29,7 @@ LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayConfig\"
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libdisplayconfig
+LOCAL_VENDOR_MODULE             := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -42,6 +44,7 @@ LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libqdMetaData
+LOCAL_VENDOR_MODULE             := true
 include $(BUILD_SHARED_LIBRARY)
 
 
